15d498d80d928daabcdf626457cb8518b9a65288
tokomac <tokomatokomac@gmail.com>
2023-05-27 16:58:23 +0900

    Initial Commit

---
+++ MusicKitApp/MusicKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicKitAppApp.swift
+//  MusicKitApp
+//
+//  Created by 木科誠 on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct MusicKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
