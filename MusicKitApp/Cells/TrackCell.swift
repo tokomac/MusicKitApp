621dddca084f48a4c23572330ff5199249bd23e6
tokomac <tokomatokomac@gmail.com>
2023-06-04 17:27:40 +0900

    Second commit

--- MusicKitApp/Cells/TrackCell.swift
+++ MusicKitApp/Cells/TrackCell.swift
@@ -12,6 +12,36 @@ protocol TrackCellTypeProtocol {
     associatedtype MusicItemType: MusicItem
     var track: MusicItemType? { get }
     var trackList: MusicItemCollection<MusicItemType>? { get }
+    var parentCollectionID: MusicItemID? { get }
+    var parentCollectionArtistName: String? { get }
+    var shouldDisplayArtwork: Bool { get }
+}
+
+private func subTitle(artistName: String?, parentCollectionArtistName: String?) -> String {
+    var subtitle = ""
+    if let artistName = artistName {
+        if artistName != parentCollectionArtistName {
+            subtitle = artistName
+        }
+    }
+    return subtitle
+}
+
+private struct MusicItemCellView: View {
+    
+    let artwork: Artwork?
+    let title: String?
+    let subtitle: String?
+    let minimumHeight: CGFloat? = 50
+    
+    var body: some View {
+        MusicItemCell(
+            artwork: artwork,
+            title: title!,
+            subtitle: subtitle
+        )
+        .frame(minHeight: minimumHeight)
+    }
 }
 
 struct SongCell: View, TrackCellTypeProtocol {
@@ -20,35 +50,21 @@ struct SongCell: View, TrackCellTypeProtocol {
 
     let track: MusicItemType?
     let trackList: MusicItemCollection<MusicItemType>?
-
-    private static let minimumHeight: CGFloat? = 50
-
     let parentCollectionID: MusicItemID?
     let parentCollectionArtistName: String?
     let shouldDisplayArtwork: Bool
 
-    private var subtitle: String {
-        var subtitle = ""
-        if track?.artistName != parentCollectionArtistName {
-            subtitle = track!.artistName
-        }
-        return subtitle
-    }
-
     var body: some View {
         Button(action: { SongPlay.shared.play(track!, in: trackList, with: parentCollectionID) }) {
-            MusicItemCell(
-                artwork: track?.artwork,
-                title: track!.title,
-                subtitle: subtitle
-            )
-            .frame(minHeight: Self.minimumHeight)
+            MusicItemCellView(artwork: track?.artwork,
+                              title: track!.title,
+                              subtitle: subTitle(artistName: track?.artistName, parentCollectionArtistName: self.parentCollectionArtistName))
         }
     }
 
-    init(_ song: MusicItemType, _ b: MusicItemCollection<MusicItemType>) {
+    init(_ song: MusicItemType, _ trackList: MusicItemCollection<MusicItemType>) {
         self.track = song
-        self.trackList = b
+        self.trackList = trackList
         self.parentCollectionID = nil
         self.parentCollectionArtistName = nil
         self.shouldDisplayArtwork = true
@@ -61,29 +77,15 @@ struct TrackCell: View, TrackCellTypeProtocol {
 
     let track: MusicItemType?
     let trackList: MusicItemCollection<MusicItemType>?
-
-    private static let minimumHeight: CGFloat? = 50
-
     let parentCollectionID: MusicItemID?
     let parentCollectionArtistName: String?
     let shouldDisplayArtwork: Bool
 
-    private var subtitle: String {
-        var subtitle = ""
-        if track?.artistName != parentCollectionArtistName {
-            subtitle = track!.artistName
-        }
-        return subtitle
-    }
-
     var body: some View {
         Button(action: { TrackPlay.shared.play(track!, in: trackList, with: parentCollectionID) }) {
-            MusicItemCell(
-                artwork: track?.artwork,
-                title: track!.title,
-                subtitle: subtitle
-            )
-            .frame(minHeight: Self.minimumHeight)
+            MusicItemCellView(artwork: track?.artwork,
+                              title: track!.title,
+                              subtitle: subTitle(artistName: track?.artistName, parentCollectionArtistName: self.parentCollectionArtistName))
         }
     }
 
