53af4d4349a1b317cb3fec905eb6c577adcd4d27
tokomac <tokomatokomac@gmail.com>
2023-06-04 17:27:52 +0900

    Second commit

--- MusicKitApp/Cells/NowPlayingCell.swift
+++ MusicKitApp/Cells/NowPlayingCell.swift
@@ -5,8 +5,50 @@
 //  Created by tokomac
 //
 
+import Combine
 import MusicKit
 import SwiftUI
+
+struct NowPlayingCell: View {
+
+    @ObservedObject var musicType = MusicType.shared
+
+    var body: some View {
+        switch musicType.musicType {
+        case .Song, .Track:
+            NowPlayingDefaultCell(musicType: musicType)
+        case .none:
+            EmptyView()
+        }
+    }
+}
+
+private struct NowPlayingDefaultCell: View {
+
+    @ObservedObject var musicType: MusicType
+    @State private var isExpanded = false
+
+    var body: some View {
+        HStack {
+            switch musicType.musicType {
+            case .Song:
+                NowPlaySongCell(music: SongPlay.shared)
+            case .Track:
+                NowPlayTrackCell(music: TrackPlay.shared)
+            case .none:
+                EmptyView()
+            }
+        }
+        .frame(height: 42)
+        .accentColor(.black)
+        .padding(.horizontal, 20)
+        .padding(.vertical, 8)
+        .contentShape(Rectangle())
+        .onTapGesture {
+            isExpanded = true
+        }
+    }
+}
 
 private struct ArtWorkCell: View {
     
@@ -47,72 +89,67 @@ private struct titleCell: View {
     }
 }
 
-struct NowPlayCell: View {
-
-    @ObservedObject private var songPlay = SongPlay.shared
-    @ObservedObject private var trackPlay = TrackPlay.shared
-    @ObservedObject private var musicType = MusicType.shared
+private struct playButtonImage: View {
     
-    @State private var isExpanded = false
+    let isPlaying: Bool
     
     var body: some View {
-        if musicType.musicType == MusicTypeEnum.Song {
-            if let track = songPlay.track {
-                HStack {
-                    ArtWorkCell(artWork: track.artwork)
-                    titleCell(title: track.title)
-                    Spacer()
-                    Button(action: {
-                        songPlay.isPlaying ? songPlay.playStop() : songPlay.PlayTrack(songPlay.track!)
-                    }) {
-                        Image(systemName: songPlay.isPlaying ? "pause.fill" : "play.fill")
-                            .font(.system(size: 22))
-                    }
-                    .padding(.trailing, 8)
-                    Button(action: {
-                        songPlay.skipToNextEntry()
-                    }) {
-                        Image(systemName: "forward.fill")
-                            .font(.system(size: 22))
-                    }
-                }
-                .frame(height: 42)
-                .accentColor(.black)
-                .padding(.horizontal, 20)
-                .padding(.vertical, 8)
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    isExpanded = true
-                }
+        Image(systemName: isPlaying ? "pause.fill" : "play.fill")
+            .font(.system(size: 22))
+    }
+}
+
+private struct skipButtonImage: View {
+    
+    var body: some View {
+        Image(systemName: "forward.fill")
+            .font(.system(size: 22))
+    }
+}
+
+private struct NowPlaySongCell: View {
+
+    @ObservedObject var music: SongPlay
+
+    var body: some View {
+        if let track = music.track {
+            ArtWorkCell(artWork: track.artwork)
+            titleCell(title: track.title)
+            Spacer()
+            Button(action: {
+                music.isPlaying ? music.playStop() : music.PlayTrack(track)
+            }) {
+                playButtonImage(isPlaying: music.isPlaying)
             }
-        } else {
-            if let track = trackPlay.track {
-                HStack {
-                    ArtWorkCell(artWork: track.artwork)
-                    titleCell(title: track.title)
-                    Spacer()
-                    Button(action: {
-                        trackPlay.isPlaying ? trackPlay.playStop() : trackPlay.PlayTrack(trackPlay.track!)
-                    }) {
-                        Image(systemName: trackPlay.isPlaying ? "pause.fill" : "play.fill")
-                            .font(.system(size: 22))
-                    }
-                    .padding(.trailing, 8)
-                    Button(action: {
-                        trackPlay.skipToNextEntry()
-                    }) {
-                        Image(systemName: "forward.fill")
-                            .font(.system(size: 22))
-                    }
-                }
-                .frame(height: 42)
-                .accentColor(.black)
-                .padding(.horizontal, 20)
-                .padding(.vertical, 8)
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    isExpanded = true
-                }
+            .padding(.trailing, 8)
+            Button(action: {
+                music.skipToNextEntry()
+            }) {
+                skipButtonImage()
+            }
+        }
+    }
+}
+
+private struct NowPlayTrackCell: View {
+
+    @ObservedObject var music: TrackPlay
+    
+    var body: some View {
+        if let track = music.track {
+            ArtWorkCell(artWork: track.artwork)
+            titleCell(title: track.title)
+            Spacer()
+            Button(action: {
+                music.isPlaying ? music.playStop() : music.PlayTrack(track)
+            }) {
+                playButtonImage(isPlaying: music.isPlaying)
+            }
+            .padding(.trailing, 8)
+            Button(action: {
+                music.skipToNextEntry()
+            }) {
+                skipButtonImage()
             }
         }
     }
